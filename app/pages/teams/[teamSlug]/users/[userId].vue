465787943ec002d5104e259751b0635f35ff19d2
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-07-23 16:44:18 +0300

    Динамические урлы

---
+++ app/pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,14 @@
+<script setup>
+const route = useRoute();
+const message = computed(() => `Computed message: user team: ${route.params.teamSlug}; user id: ${route.params.userId}`);
+</script>
+<template>
+  <div>
+    <p>
+      Data in template:
+      {{ $route.params.teamSlug }}
+      {{ $route.params.userId }}
+    </p>
+    <p>{{message}}</p>
+  </div>
+</template>
