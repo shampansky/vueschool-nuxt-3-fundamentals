e442aeaf2220c8aa9b2647bd9c3df70d9b7aa568
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-08-25 16:27:13 +0300

    Использование useFetch вместо useAsyncData

--- app/pages/movies/[id].vue
+++ app/pages/movies/[id].vue
@@ -1,5 +1,10 @@
 <script setup>
 const route = useRoute();
+const { data, status } = useFetch(`http://www.omdbapi.com/?apikey=876937fd&i=${route.params.id}`, {
+  pick: ['Title', 'Plot'],
+  key: `/movies/${route.params.id}`,
+})
+/*
 const { data, status } = useAsyncData(
   `/movies/${route.params.id}`,
   () => {
@@ -17,11 +22,12 @@ const { data, status } = useAsyncData(
     // },
   }
 );
+*/
 </script>
 <template>
   <div>
     {{ data }}
-    <br />
+    <br>
     {{ status }}
   </div>
 </template>
