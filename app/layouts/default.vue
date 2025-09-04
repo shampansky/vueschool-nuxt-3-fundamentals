5de9fb2b9adb1c80e78a1bc7ffa5b716f3af68c7
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-09-04 16:47:23 +0300

    Использование useState и комопзабла для хранения и изменения состояния.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,14 @@
+<script setup>
+const isLoggedIn = useIsLoggedIn()
+</script>
 <template>
   <div>
     <nav>
       <NuxtLink to="/movies">Movies</NuxtLink>
       <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="https://google.com">Google</NuxtLink>
+      <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+      <a v-else href="#" @click.prevent="isLoggedIn = false">Logout</a>
     </nav>
     <slot />
   </div>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,13 +1,19 @@
 <script setup>
+const isLoggedIn = useIsLoggedIn()
 definePageMeta({
   layout: 'plain',
 })
+
+function login() {
+  isLoggedIn.value = true
+  navigateTo('/')
+}
 </script>
 
 <template>
   <div>
     <h1>Login</h1>
-    <form>
+    <form @submit.prevent="login">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
       <button type="submit">Login</button>
